6541555cefb273b579d7597539074c4a64ff5aed
Matt Gorzka <matt.gorzka@eolianenergy.com>
2023-08-11 16:51:39 -0700

    labels fix

--- src/label.js
+++ src/label.js
@@ -56,13 +56,13 @@ function getScaleOrigin(el, context) {
 }
 
 function getPositioner(el) {
-  if (el instanceof ArcElement) {
+  if (el.constructor.id == "arc") {
     return positioners.arc;
   }
-  if (el instanceof PointElement) {
+  if (el.constructor.id == "point") {
     return positioners.point;
   }
-  if (el instanceof BarElement) {
+  if (el.constructor.id == "bar") {
     return positioners.bar;
   }
   return positioners.fallback;
